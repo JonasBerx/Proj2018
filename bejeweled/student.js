2fc600ad21f4898e0c10631dbaca8ea75aedf91c
JonasBerx <43341402+JonasBerx@users.noreply.github.com>
2018-12-17 13:51:13 +0100

    Updated

--- bejeweled/student.js
+++ bejeweled/student.js
@@ -1,1 +1,18 @@
 // Schrijf hier je code
+function width(grid) {
+    let width = 0;
+    for (let i = 0; i < grid.length; i++) {
+        width += 1;
+
+    }
+    return width;
+}
+function heigth(grid) {
+    let height = 0;
+    for (let i = 0; i < grid.length; i++) {
+        heigth += 1;
+
+    }
+    return height;
+}
+
